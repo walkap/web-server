e777f9424f5b87ea49072d308eaaef3fd8d81f85
riccardo <riccardogamucci@alice.it>
2018-08-12 20:08:22 +0200

    logging, storage, parser modified

--- http_parsing/request_parser.h
+++ http_parsing/request_parser.h
@@ -2,78 +2,110 @@
 #define WEB_SERVER_HTTP_PARSER_H
 
 #include "../utils.h"
+#include <ctype.h>
 
 struct http_request {
 
     char *method;
     char *uri;
+    char *version;
     char *accept;
     char *user_agent;
 };
 
-void parse_request_line(struct http_request *r, char* t)
+double get_double(char *str)
 {
-    char *s = " ";
+    while (*str && !(isdigit(*str) || ((*str == '-' || *str == '+') && isdigit(*(str + 1)))))
+        str++;
+
+    return strtod(str, NULL);
+}
+
+double parse_weight(char *str)
+{
+    printf(" accept %s\n", str);
+    char* s = "q";
     char *token;
 
-    token = strtok(t, s);
-    exit_on_error(t == NULL, "error in strtok in parser_request_line");
+    token=strtok(str, s);
+    token=strtok(NULL, s);
+
+    double q = get_double(token);
+
+    return q;
+
+}
+char *parse_user_agent(char *str)
+{
+    char* s = " ";
+    char* token;
+
+    token=strtok(str, s);
+    token=strtok(NULL, s);
+
+
+    return token;
+
+}
+
+void parse_request_line(struct http_request *r, char* t)
+{
+    char *sc = " ";
+    char *token;
+
+    token = strtok(t, sc);
+    exit_on_error(t == NULL, "error in strtok in parse_request_line");
 
     if (strstr(t, "GET")!= NULL)
         r->method = "GET";
     if (strstr(token, "HEAD")!= NULL)
         r->method = "HEAD";
 
-    token = strtok(NULL, s);
-    exit_on_error(token == NULL, "error in strtok in parser_request_line");
+    token = strtok(NULL, sc);
+    exit_on_error(token == NULL, "error in strtok in parse_request_line");
 
     r->uri=token;
 
+    token = strtok(NULL, sc);
+    exit_on_error(token == NULL, "error in strtok in parse_request_line");
 
-
-}
-
-void parse_header_line(struct http_request *r, char* t, char* type)
-{
-    char *s = " ";
-    char *token;
-
-    token = strtok(t, s);
-    exit_on_error(token == NULL, "error in strtok in parser_header_line");
-
-    token = strtok(NULL, s);
-    exit_on_error(token == NULL, "error in strtok in parser_header_line");
-
-    if (strcmp(type, "user_agent")==0)
-        r->user_agent=token;
-    if (strcmp(type, "accept")==0)
-        r->accept=token;
-
+    r->version= token;
 
 }
+
 
 struct http_request* parse_request(char *str)
 {
     struct http_request* request;
-    char *s = "\n";
+    const char s[2] = "\n";
     char *token;
+
     request = malloc(sizeof(struct http_request));
     exit_on_error(request == NULL, "error in malloc");
 
-    token = strtok(str, s);
-    exit_on_error(token == NULL, "error in strtok in parse_request");
+    int len = strlen(str);
+    char *buff = malloc(len + 1);
+    exit_on_error(buff == NULL, "error in malloc");
+    strcpy(buff, str);
 
-    parse_request_line(request, token);
+        token = strtok(buff, s);
+        exit_on_error(token == NULL, "error in strtok in parse_request");
+
+        parse_request_line(request, buff);
+
+    token = strtok(str, s);
 
     while( token != NULL ) {
 
         if (strstr(token, "User-Agent:")!= NULL) {
-            parse_header_line(request, token, "user_agent");
+            request->user_agent=token;
+            printf("%s\n", request->user_agent);
         }
         if (strstr(token, "Accept:")!= NULL) {
-            parse_header_line(request, token, "accept");
-        }
+            request->accept=token;
+            printf("%s\n", request->accept);
 
+        }
 
         token = strtok(NULL, s);
     }
--- utils.h
+++ utils.h
@@ -14,11 +14,12 @@
 #include <errno.h>
 #include <asm/ioctls.h>
 #include <stropts.h>
+#include <arpa/inet.h>
 
-char* HTTP_404 = "HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=iso-8859-1\nContent-Length: 141\nConnection: Closed\n\n<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\"> <html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL  was not found on this server.</p></body></html>";
- char* HTTP_400 = "HTTP/1.1 400 Bad Request\ntext/html; charset=iso-8859-1\nContent-Length: 246\nConnection: Closed\n\n<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\"> <html><head><title>400 Bad Request</title></head><body><h1>Bad Request</h1><p>Your browser sent a request that this server could not understand.</p><p>The request line contained invalid characters following the protocol string.</p></body></html>";
- char* HTTP_INDEX = "HTTP/1.1 200 OK\nContent-Length: 205\nContent-Type: text/html\nConnection: keep-alive\n\n<!DOCTYPE HTML PUBLIC \\\"-//IETF//DTD HTML 2.0//EN\\\"> <html><head><title>Content Adaptation</title></head><body><h1>Content Adaptation: Home Page</h1><p>Inserire link delle immagini //TODO</p></body></html>";
- char* HTTP_404_1 = "HTTP/1.1 404 Not Found\nConnection: Closed\n\n";
+#define DIM_HEADER 300
+
+ char* HTML_400 = "<!DOCTYPE HTML> <html><head><title>400 Bad Request</title></head><body><h1>Bad Request</h1><p>Your browser sent a request that this server could not understand.</p><p>The request line contained invalid characters following the protocol string.</p></body></html>";
+ char* HTML_INDEX = "<!DOCTYPE HTML> <html><head><title>Content Adaptation</title></head><body><h1>Content Adaptation: Home Page</h1><p><a href=\"/wizard.jpg\">wizard.jpg</a></p></body></html>";
 
 void exit_on_error (int condition, const char * message)
 {
