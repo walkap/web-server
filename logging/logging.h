e880e6653e35250b1e4b6d6043bc206d1519fb91
roberto <roberto.capannelli@gmail.com>
2018-08-21 19:06:12 +0200

    fixed logfile in the root, untracked the older and new one logfile (IT MUST NOT BE IN THE REPO)

--- logging/logging.h
+++ logging/logging.h
@@ -30,7 +30,7 @@ void logging(struct http_request* pt, char *str, int len)
 
     char* user_agent = parse_user_agent(pt->user_agent);
 
-    FILE *ptr = open_logfile("../logging/logfile");
+    FILE *ptr = open_logfile("../logfile");
 
     fprintf(ptr, "%s %s %s  %d-%d-%d %d:%d:%d   %s   %s    %i\n", pt->method,
                pt->uri, pt->version, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, user_agent, response, len);
