261a9d10d542af2e8b6d68a8cfed8c6eb5fe8c36
roberto <roberto.capannelli@gmail.com>
2018-09-03 18:43:29 +0200

    optimized includes

--- ua_parsing/ua_parsing.h
+++ ua_parsing/ua_parsing.h
@@ -1,9 +1,9 @@
 #ifndef WEB_SERVER_USER_AGENT_PARSING_H
 #define WEB_SERVER_USER_AGENT_PARSING_H
 
-#include <stdlib.h>
-#include <string.h>
+#include "../utils.h"
 #include "src/trie/51Degrees.h"
+
 #define UA_PARSING_DEBUG 0
 #define DATA_PATH "../ua_parsing/src/51Degrees-LiteV3.4.trie";
 #define PROPERTIES "ScreenPixelsWidth,ScreenPixelsHeight,PlatformName,IsMobile"
