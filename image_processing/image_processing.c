347a8a565884855e582540daf4f0108bbf4b46d8
roberto <roberto.capannelli@gmail.com>
2018-09-12 19:28:50 +0200

    back to jpg and fixed the index.html

--- image_processing/image_processing.c
+++ image_processing/image_processing.c
@@ -103,7 +103,7 @@ char *rename_file(const char *filename, size_t width) {
     //Copy the file name without the format into the array char
     strncpy(new_filename, filename, strlen(filename) - 4);
     //Add the image size and formate to the new file name array char
-    if( sprintf(new_filename + strlen(filename) - 4, "-%iw.png", (int) width) < 0){
+    if( sprintf(new_filename + strlen(filename) - 4, "-%iw.jpg", (int) width) < 0){
         perror("sprintf");
         exit(EXIT_FAILURE);
     }
