45cefea66f71497c3e18cb0e8abc535097dba6d4
roberto <roberto.capannelli@gmail.com>
2018-09-16 20:56:27 +0200

    deleted hard coded string

--- ua_parsing/ua_parsing.h
+++ ua_parsing/ua_parsing.h
@@ -11,9 +11,4 @@
 
 int get_screen_size_ua(const char *ua_str, int *info);
 
-//TODO this is just for testing should be deleted as soon as we can
-/*const char* mobileUserAgent = ("Mozilla/5.0 (iPhone; CPU iPhone OS 7_1 like Mac OS X) "
-                               "AppleWebKit/537.51.2 (KHTML, like Gecko) 'Version/7.0 Mobile/11D167 "
-                               "Safari/9537.53");*/
-
 #endif //WEB_SERVER_USER_AGENT_PARSING_H
